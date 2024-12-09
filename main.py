69a94cb84f8b88a8c1067aab5dc13bbff47c56cb
Maxim <mp9097978@gmail.com>
2024-12-09 11:51:57 +0300

    work3

--- main.py
+++ main.py
@@ -126,3 +126,113 @@ best_student.rate_lecturer(cool_lecturer, 'Python', 10)
 # Проверяем результаты 
 print(f'Оценки студента {best_student.name}: {best_student.grades}')
 print(f'Оценки лектора {cool_lecturer.name}: {cool_lecturer.grades}')
+
+#Задача 3.
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course].append(grade)
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def average_grade(self):
+        total_grades = 0
+        total_courses = 0
+        for grades in self.grades.values():
+            total_grades += sum(grades)
+            total_courses += len(grades)
+        return total_grades / total_courses if total_courses > 0 else 0
+
+    def __str__(self):
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}\n'
+                f'Средняя оценка за домашние задания: {self.average_grade():.1f}\n'
+                f'Курсы в процессе изучения: {", ".join(self.courses_in_progress) if self.courses_in_progress else "Нет"}\n'
+                f'Завершенные курсы: {", ".join(self.finished_courses) if self.finished_courses else "Нет"}')
+
+    def __lt__(self, other):
+        if isinstance(other, Student):
+            return self.average_grade() < other.average_grade()
+        return NotImplemented
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+    def average_grade(self):
+        total_grades = 0
+        total_courses = 0
+        for grades in self.grades.values():
+            total_grades += sum(grades)
+            total_courses += len(grades)
+        return total_grades / total_courses if total_courses > 0 else 0
+
+    def __str__(self):
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}\n'
+                f'Средняя оценка за лекции: {self.average_grade():.1f}')
+
+    def __lt__(self, other):
+        if isinstance(other, Lecturer):
+            return self.average_grade() < other.average_grade()
+        return NotImplemented
+
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course].append(grade)
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
+
+# Создаем студентов, лекторов и проверяющих
+student1 = Student("Ruoy", "Eman", "male")
+student1.courses_in_progress.append("Python")
+student1.finished_courses.append("Введение в программирование")
+
+lecturer1 = Lecturer("Some", "Buddy")
+lecturer1.courses_attached.append("Python")
+
+reviewer1 = Reviewer("Alice", "Smith")
+reviewer1.courses_attached.append("Python")
+
+# Выставляем оценки
+reviewer1.rate_hw(student1, "Python", 10)
+reviewer1.rate_hw(student1, "Python", 9)
+
+lecturer1.grades["Python"] = [8, 9, 10]
+
+# Печатаем информацию
+print(student1)
+print(lecturer1)
+print(reviewer1)
+
+# Сравнение студентов и лекторов
+print(student1 < Student("John", "Doe", "male"))  # False
+print(lecturer1 < Lecturer("Another", "Lecturer"))  # Сравнение лекторов аналогично
+
