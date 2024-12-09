3143b8a8cdaf327daf04746b8610c51359ac5e50
Maxim <mp9097978@gmail.com>
2024-12-09 11:44:14 +0300

    work2

--- main.py
+++ main.py
@@ -59,3 +59,70 @@ cool_lecturer.rate_hw(best_student, 'Python', 8)
 cool_reviewer.review_hw(best_student, 'Python', 9)
 
 print(best_student.grades)
+
+
+#Задача 2.
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+    
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course].append(grade)
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course].append(grade)
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+# Пример использования
+
+# Создаем студента
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+# Создаем лектора и прикрепляем курс
+cool_lecturer = Lecturer('Some', 'Buddy')
+cool_lecturer.courses_attached += ['Python']
+
+# Создаем рецензента (Reviewer) и прикрепляем курс
+cool_reviewer = Reviewer('John', 'Doe')
+cool_reviewer.courses_attached += ['Python']
+
+# Рецензент выставляет оценки студенту
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 8)
+
+# Студент ставит оценки лектору
+best_student.rate_lecturer(cool_lecturer, 'Python', 9)
+best_student.rate_lecturer(cool_lecturer, 'Python', 10)
+
+# Проверяем результаты 
+print(f'Оценки студента {best_student.name}: {best_student.grades}')
+print(f'Оценки лектора {cool_lecturer.name}: {cool_lecturer.grades}')
